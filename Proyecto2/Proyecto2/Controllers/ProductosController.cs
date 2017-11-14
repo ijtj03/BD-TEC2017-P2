4937a97a621e754cb094e3361826c02da5d887dd
Esteban Herrera V <nabetse.hv.09@gmail.com>
2017-11-13 22:32:41 -0600

    Este es el proyecto de Visual

---
+++ Proyecto2/Proyecto2/Controllers/ProductosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Proyecto2DataAccess;
+
+namespace Proyecto2.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        public IEnumerable<productos> GetAllProductos()
+        {
+            using (Proyecto2Entities entities = new Proyecto2Entities())
+            {
+                entities.Configuration.LazyLoadingEnabled = false;
+                return entities.productos.ToList();
+            }
+        }
+
+        public productos GetProducto(int id)
+        {
+            using (Proyecto2Entities entities = new Proyecto2Entities())
+            {
+                entities.Configuration.LazyLoadingEnabled = false;
+                return entities.productos.FirstOrDefault(e => e.idproducto == id);
+            }
+        }
+
+
+    }
+}
---
+++ Proyecto2/Proyecto2/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Proyecto2
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //Evito las referencias circulares al trabajar con Entity FrameWork         
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            //Elimino que el sistema devuelva en XML, sólo trabajaremos con JSON
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
+
+            /*GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);*/
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
